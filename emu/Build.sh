a4d32e19b2536dfc92b5e0abcf77aa4044a790db
fffooofoooo <111528532+fffooofoooo@users.noreply.github.com>
2026-02-17 13:11:37 -0600

    Updated Build.sh to the proper config for the master tests

--- emu/Build.sh
+++ emu/Build.sh
@@ -11,6 +11,6 @@ grep error: make-install-framework.log
 #### ME11 test configuratoion (modified by Ange)
 #xdaq.exe -h 10.119.64.4 -p 20016 -e $PROJECT_ROOT/xml/default.profile_cc7_noXMAS  -c $PROJECT_ROOT/xml/tamu_test2019_GEM.xml
 ####ME21 test configuration
-xdaq.exe -h 10.119.64.4 -p 20016 -e $PROJECT_ROOT/xml/default.profile_cc7_noXMAS -c $PROJECT_ROOT/xml/tamu_test2021_MEX1_SLHCon.xml
-#xdaq.exe -h 10.119.64.4 -p 20016 -e $PROJECT_ROOT/xml/default.profile_cc7_noXMAS  -c $PROJECT_ROOT/xml/tamu_test2019_GEM.xml
+#xdaq.exe -h 10.119.64.4 -p 20016 -e $PROJECT_ROOT/xml/default.profile_cc7_noXMAS -c $PROJECT_ROOT/xml/tamu_test2021_MEX1_SLHCon.xml
+xdaq.exe -h 10.119.64.4 -p 20016 -e $PROJECT_ROOT/xml/default.profile_cc7_noXMAS -c $PROJECT_ROOT/xml/tamu_test2019_GEM.xml
 #Set=framework make -j 8 clean
